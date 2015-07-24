4239d5a4463796e430d4d041f059d5463da2dac6
yamiross <dark_git8>
2015-07-24 15:58:50 +0300

    added bean lLifeCycle management to context

--- src/main/java/com/epam/pizza/infrastructure/JavaConfigApplicationContext.java
+++ src/main/java/com/epam/pizza/infrastructure/JavaConfigApplicationContext.java
@@ -1,6 +1,7 @@
 package com.epam.pizza.infrastructure;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,25 +19,63 @@ public class JavaConfigApplicationContext implements ApplicationContext {
 			return bean;
 		}
 		
-		Class<?> clazz = config.getImplementation(beanName);
-		Constructor<?> constructor = clazz.getConstructors()[0]; 
-		Class<?>[] paramTypes = constructor.getParameterTypes();
-		if (paramTypes.length == 0) {
-			bean = clazz.newInstance();
-			beans.put(beanName, bean);
-			return bean;
+		BeanBuilder beanBuilder = new BeanBuilder(beanName);
+		beanBuilder.createObject();
+		beanBuilder.createProxy();
+		beanBuilder.callInitMethod();
+		
+		return beanBuilder.getObject();
+	}
+	
+	class BeanBuilder {
+		
+		private Object obj;
+		private Class cl;
+		private String beanName;
+		
+		public BeanBuilder(String beanName) {
+			this.beanName = beanName;
+		}
+
+		public void createObject() throws Exception {
+			Class<?> clazz = config.getImplementation(beanName);
+			Constructor<?> constructor = clazz.getConstructors()[0]; 
+			Class<?>[] paramTypes = constructor.getParameterTypes();
+			if (paramTypes.length == 0) {
+				obj = clazz.newInstance();
+				beans.put(beanName, obj);
+				return;
+			}
+			
+			Object[] args = new Object[paramTypes.length];
+			String className;
+			String argBeanName;
+			for (int i = 0; i < paramTypes.length; i++) {
+				className = paramTypes[i].getSimpleName();
+				argBeanName = className.substring(0, 1).toLowerCase() + className.substring(1);
+				args[i] = getBean(argBeanName);
+			}
+			obj = constructor.newInstance(args);
+			beans.put(beanName, obj);
 		}
 		
-		Object[] args = new Object[paramTypes.length];
-		String className;
-		String argBeanName;
-		for (int i = 0; i < paramTypes.length; i++) {
-			className = paramTypes[i].getSimpleName();
-			argBeanName = className.substring(0, 1).toLowerCase() + className.substring(1);
-			args[i] = getBean(argBeanName);
+		public void callInitMethod() throws Exception {
+			cl = obj.getClass();
+			Method method;
+			try {
+				method = cl.getMethod("init");
+			} catch (NoSuchMethodException ignore) {
+				return;
+			}
+			method.invoke(obj);
 		}
-		bean = constructor.newInstance(args);
-		beans.put(beanName, bean);
-		return bean;
+		
+		public void createProxy() {
+			// TODO Auto-generated method stub
+		}
+		
+		public Object getObject() {
+			return obj;
+		}
 	}
 }
